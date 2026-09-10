82b200e60cad341ddb014fab25ee50d15e6163d8
Anthony Miranda <ant1982@gmail.com>
2026-09-09 20:24:02 -0700

    Restore working game domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   preview: {
-    allowedHosts: ["games.badantproductions.com"],
+    allowedHosts: ["isaac.badantproductions.com"],
   },
 });
